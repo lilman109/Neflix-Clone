25e662091960db0483df25f3cbd8fa165c0ee71b
lilman109 <akira.yoshidasjsu@gmail.com>
2021-05-01 14:41:32 +0900

    added jumbotron

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import jumbotronData from './fixtures/jumbo.json';
+import { Jumbotron } from './components/jumbotron/index';
 
 const App = () => {
-	return <p>Hello</p>;
+	return (
+		<Jumbotron.Container>
+			{jumbotronData.map((item) => {
+				return (
+					<Jumbotron key={item.id} direction={item.direction}>
+						<Jumbotron.Pane>
+							<Jumbotron.Title>{item.title}</Jumbotron.Title>
+							<Jumbotron.SubTitle>{item.subTitle}</Jumbotron.SubTitle>
+						</Jumbotron.Pane>
+						<Jumbotron.Pane>
+							<Jumbotron.Image src={item.image} alt={item.alt} />
+						</Jumbotron.Pane>
+					</Jumbotron>
+				);
+			})}
+		</Jumbotron.Container>
+	);
 };
 
 export default App;
