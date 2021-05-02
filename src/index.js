76bd4a72cba1f170da615554272a51a2be21c296
lilman109 <akira.yoshidasjsu@gmail.com>
2021-05-02 21:08:07 +0900

    initialized firebase

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@ import { render } from 'react-dom';
 import 'normalize.css';
 import App from './app';
 import { GlobalStyles } from './global-styles';
+import { firebaseApp } from './lib/firebase.prod';
+import { FirebaseContext } from './context/firebase';
 
 render(
 	<>
-		<GlobalStyles />
-		<App />
+		<FirebaseContext.Provider value={{ firebaseApp }}>
+			<GlobalStyles />
+			<App />
+		</FirebaseContext.Provider>
 	</>,
 	document.getElementById('root')
 );
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,16 @@
+import Firebase from 'firebase';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+	apiKey: 'AIzaSyACvUD32NjM_VB_I_b1Kfq_MBDDesixicU',
+	authDomain: 'netflix-clone-10890.firebaseapp.com',
+	projectId: 'netflix-clone-10890',
+	storageBucket: 'netflix-clone-10890.appspot.com',
+	messagingSenderId: '532232109057',
+	appId: '1:532232109057:web:57f808c2db9c28e20c1400',
+};
+
+const firebaseApp = Firebase.initializeApp(config);
+
+export { firebaseApp };
