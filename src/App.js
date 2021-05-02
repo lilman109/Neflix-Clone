ae19e5ae489385b42a0005ea7793f46ec7f77201
lilman109 <akira.yoshidasjsu@gmail.com>
2021-05-02 12:42:49 +0900

    added routes and temp page components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './pages/home';
+import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/route';
 
 const App = () => {
 	return (
 		<Router>
-			<Switch>
-				<Route exact path={ROUTES.HOME} component={Home} />
-			</Switch>
+			<Route exact path={ROUTES.HOME} component={Home} />
+			<Route path={ROUTES.BROWSE} component={Browse} />
+			<Route exact path={ROUTES.SIGN_IN} component={SignIn} />
+			<Route exact path={ROUTES.SIGN_UP} component={SignUp} />
 		</Router>
 	);
 };
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { default as Home } from './home';
+export { default as Browse } from './browse';
+export { SignUp } from './signup';
+export { SignIn } from './signin';
