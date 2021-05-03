f593c5188b2c1ef594a49392d87431216bd8a9f6
lilman109 <akira.yoshidasjsu@gmail.com>
2021-05-04 00:08:06 +0900

    modified routing for logged in and logged out user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { IsUserRedirect, ProtectedRoute } from './helpers/routes';
+import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import { Home, Browse, SignIn, SignUp } from './pages';
 import * as ROUTES from './constants/route';
 
 const App = () => {
+	const user = { name: 'foo' };
 	return (
 		<Router>
 			<Switch>
-				<Route exact path={ROUTES.HOME} component={Home} />
-				<Route path={ROUTES.BROWSE} component={Browse} />
-				<Route path={ROUTES.SIGN_IN} component={SignIn} />
-				<Route path={ROUTES.SIGN_UP} component={SignUp} />
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.SIGN_IN}
+					exact
+				>
+					<SignIn />
+				</IsUserRedirect>
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.SIGN_UP}
+					exact
+				>
+					<SignUp />
+				</IsUserRedirect>
+				<IsUserRedirect
+					user={user}
+					loggedInPath={ROUTES.BROWSE}
+					path={ROUTES.HOME}
+					exact
+				>
+					<Home />
+				</IsUserRedirect>
+				<ProtectedRoute user={user} path={ROUTES.BROWSE}>
+					<Browse />
+				</ProtectedRoute>
 			</Switch>
 		</Router>
 	);
--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,9 +1,98 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import { FirebaseContext } from '../context/firebase';
+import { HeaderContainer } from '../containers/header';
+import { Form } from '../components';
+import { FooterContainer } from '../containers/footer';
+import * as ROUTES from '../constants/route';
 
 export const SignUp = () => {
+	const history = useHistory();
+	const { firebaseApp } = useContext(FirebaseContext);
+
+	const [firstName, setFirstname] = useState('');
+	const [email, setEmail] = useState('');
+	const [password, setPassword] = useState('');
+	const [error, setError] = useState('');
+
+	const handleSignUp = (event) => {
+		event.preventDefault();
+
+		firebaseApp
+			.auth()
+			.createUserWithEmailAndPassword(email, password)
+			.then((result) =>
+				result.user
+					.updateProfile({
+						displayName: firstName,
+						photoURL: Math.floor(Math.random() * 5) + 1,
+					})
+					.then(() => {
+						history.push(ROUTES.BROWSE);
+					})
+			)
+			.catch((error) => {
+				setFirstname('');
+				setEmail('');
+				setPassword('');
+				setError(error.message);
+			});
+	};
+
+	const handleFirstNameChange = (event) => {
+		setFirstname(event.target.value);
+	};
+
+	const handleEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
+
+	const handlePasswordChange = (event) => {
+		setPassword(event.target.value);
+	};
+
 	return (
-		<div>
-			<h1>Sign up!</h1>
-		</div>
+		<>
+			<HeaderContainer>
+				<Form>
+					<Form.Title>Sign Up</Form.Title>
+					{error && <Form.Error>{error}</Form.Error>}
+					<Form.Base onSubmit={handleSignUp} method='POST'>
+						<Form.Input
+							onChange={handleFirstNameChange}
+							value={firstName}
+							placeholder='First Name'
+						></Form.Input>
+						<Form.Input
+							onChange={handleEmailChange}
+							value={email}
+							placeholder='Email Address'
+						></Form.Input>
+						<Form.Input
+							onChange={handlePasswordChange}
+							value={password}
+							placeholder='Password'
+							type='password'
+							autocomplete='off'
+						></Form.Input>
+						<Form.Submit
+							disabled={!email || !password || !firstName}
+							type='submit'
+						>
+							Sign In
+						</Form.Submit>
+					</Form.Base>
+					<Form.Text>
+						New to Netflix?&nbsp;
+						<Form.Link>Sign Up Now</Form.Link>
+					</Form.Text>
+					<Form.TextSmall>
+						This page is proctected by Google reCAPTCHA to ensure you're now a
+						bot. Learn more.
+					</Form.TextSmall>
+				</Form>
+			</HeaderContainer>
+			<FooterContainer></FooterContainer>
+		</>
 	);
 };
