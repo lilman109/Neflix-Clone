b68eb24125ab3beeb80515294ba153c8aeb707d0
lilman109 <akira.yoshidasjsu@gmail.com>
2021-05-09 22:07:26 +0900

    added the header in browse page

--- src/components/loading/index.js
+++ src/components/loading/index.js
@@ -3,13 +3,13 @@ import { Spinner, LockBody, Photo, ReleaseBody } from './styles/loading';
 
 export const Loading = ({ src, ...restProps }) => {
 	return (
-		<Spinner>
+		<Spinner {...restProps}>
 			<LockBody />
 			<Photo src={`/images/users/${src}.png`} />
 		</Spinner>
 	);
 };
 
-Loading.ReleaseBody = function LoadingReleaseBody({ children, ...restProps }) {
-	return <ReleaseBody {...restProps}>{children}</ReleaseBody>;
+Loading.ReleaseBody = function LoadingReleaseBody() {
+	return <ReleaseBody />;
 };
